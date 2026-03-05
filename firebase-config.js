de438df0b6c67cc6400c57ec54ab352738d63822
nazarenowc-star <nazarenowc@gmail.com>
2026-03-05 09:33:47 -0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDEotosWG9ss-pMDbZluy5Dg_bOfG8kHJQ",
+  authDomain: "rdo-sync.firebaseapp.com",
+  projectId: "rdo-sync",
+  storageBucket: "rdo-sync.firebasestorage.app",
+  messagingSenderId: "551344630048",
+  appId: "1:551344630048:web:87e485e712473a46234f45"
+};
